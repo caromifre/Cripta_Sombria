4080c3c14bed612021693f6f92f467ef1faed1d9
Emiliano Pellegrino <epellegrino@gmail.com>
2024-11-19 08:39:08 -0300

    Menu principal y ajustes estéticos en HUD
    Armado de escena para Menu principal con botón de Play, Salir y Creditos funcionando. Cambio de avatar y color de fondo de inventario en HUD

---
+++ Assets/Script/Huds/MenuInicial.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+
+    public void Jugar()
+    {
+        SceneManager.LoadScene("Nivel1");
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Salir...");
+        Application.Quit();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
