b5feb49166d1e8078394664bc7120b320820c2db
Emiliano Pellegrino <epellegrino@gmail.com>
2024-11-20 00:46:32 -0300

    Nueva estética pantalla Has Ganado
    Cambio de estética de fondo y botón Replay

--- Assets/Script/Huds/MenuInicial.cs
+++ Assets/Script/Huds/MenuInicial.cs
@@ -17,16 +17,4 @@ public class MenuInicial : MonoBehaviour
         Application.Quit();
     }
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
