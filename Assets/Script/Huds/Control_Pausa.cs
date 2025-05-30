2b9d4a2cfe7a0816ceb4a4ab5cfc91173cb8c888
caromifre <kafmisio@gmail.com>
2025-05-29 22:18:28 -0300

    correccion bug en el menu de Pausar_Juego

--- Assets/Script/Huds/Control_Pausa.cs
+++ Assets/Script/Huds/Control_Pausa.cs
@@ -6,7 +6,7 @@ using static Constantes_celda;
 
 public class Control_Pausa : MonoBehaviour
 {
-    [SerializeField] GameObject _menuPausa; // Referencia al menú de pausa (UI)
+    [SerializeField] GameObject _menuPausa; // Referencia al menï¿½ de pausa (UI)
     [SerializeField] AudioSource _audioFuente; // Referencia al audio principal del juego
     [SerializeField] Button _btn_inicio, _volver_a_jugar,_btn_salir;
     Game_manager _controler;
@@ -18,7 +18,7 @@ public class Control_Pausa : MonoBehaviour
         _controler = Game_manager.Instance;
         _volver_a_jugar?.onClick.AddListener(Reanudar_Juego);
         _btn_salir?.onClick.AddListener(Salir);
-        _btn_inicio?.onClick.AddListener(_controler.mostrar_menu_incio);
+        _btn_inicio?.onClick.AddListener(Mostrar_inicio);
     }
 
     void Update()
@@ -41,7 +41,7 @@ public class Control_Pausa : MonoBehaviour
     {
         _juego_Pausado = true;
 
-        // Mostrar el menú de pausa si existe
+        // Mostrar el menï¿½ de pausa si existe
         if (_menuPausa != null){
             _controler.ocultar_hud();
             _menuPausa.SetActive(true);
@@ -57,7 +57,7 @@ public class Control_Pausa : MonoBehaviour
     {
         _juego_Pausado = false;
 
-        // Ocultar el menú de pausa si existe
+        // Ocultar el menï¿½ de pausa si existe
         if (_menuPausa != null){
             _menuPausa.SetActive(false);
             _controler.agregar_escena();//mostrar hud
@@ -68,6 +68,11 @@ public class Control_Pausa : MonoBehaviour
         Time.timeScale = 1f;
         if (_audioFuente != null)
             _audioFuente.UnPause();
+    }
+
+    public void Mostrar_inicio()
+    {
+        _controler.mostrar_menu_incio();
     }
 
     public void Salir()
